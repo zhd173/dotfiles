b5e274d64badedca93a8ae62e70698cdfdfc9d64
Haidong <zhd173@outlook.com>
2018-05-25 15:52:16 +0800

    remove compose

--- bootstrap-fish.fish
+++ bootstrap-fish.fish
@@ -1,10 +1,6 @@
 #!/usr/local/bin/fish
 curl -Lo ~/.config/fish/functions/fisher.fish --create-dirs https://git.io/fisher
 
-curl https://raw.githubusercontent.com/docker/compose/1.21.2/contrib/completion/fish/docker-compose.fish > ~/.config/fish/completions/docker-compose.fish
-curl https://raw.githubusercontent.com/docker/cli/master/contrib/completion/fish/docker.fish > ~/.config/fish/completions/docker.fish
-
-fisher docker-completion
 fisher pipenv
 fisher laughedelic/pisces
 fisher laughedelic/brew-completions
@@ -25,7 +21,6 @@ fisher omf/ssh
 fisher omf/python
 fisher omf/git-flow
 fisher omf/django
-fisher omf/composer
 fisher omf/bundler
 fisher omf/brew
 fisher omf/theme-bobthefish
